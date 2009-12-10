6845492f6f85ee875e31a9e72f8e20db7aa61998
Charlie Knudsen <charliek@charliek-desktop.(none)>
2009-12-10 02:06:02 -0600

    initial commit

---
+++ src/main/scala/net/cknudsen/projects/scalatest/Main.scala
@@ -0,0 +1,12 @@
+package net.cknudsen.projects.scalatest
+
+object Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
